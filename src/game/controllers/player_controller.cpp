c80c3e653cf93b0bf30bc6f848a1a73e5a76ccf1
Jonathan DiGiacomo <jonathan.digiacomo@gmail.com>
2015-11-23 08:42:54 -0500

    Add rudimentary acceleration based player controller.

---
+++ src/game/controllers/player_controller.cpp
@@ -0,0 +1,155 @@
+// player_controller.cpp
+#include "player_controller.h"
+
+namespace StevensDev
+{
+
+namespace mgc
+{
+
+// CONSTANT DECLARATIONS
+constexpr float PlayerController::MAX_OFFSET;
+constexpr float PlayerController::MIN_VELOCITY;
+constexpr float PlayerController::ACCELERATION;
+constexpr float PlayerController::KINETIC_FRICTION;
+constexpr float PlayerController::STATIC_FRICTION;
+
+// MEMBER FUNCTIONS
+void PlayerController::preTick()
+{
+    using namespace sgdi;
+    using namespace gel::math;
+
+    Input& in = Input::inst();
+
+    float vertical = 0.0f;
+    float horizontal = 0.0f;
+
+    if ( in.isDown( Input::KEY_W ) )
+    {
+        vertical -= 1.0f;
+    }
+
+    if ( in.isDown( Input::KEY_S ) )
+    {
+        vertical += 1.0f;
+    }
+
+    if ( in.isDown( Input::KEY_A ) )
+    {
+        horizontal -= 1.0f;
+    }
+
+    if ( in.isDown( Input::KEY_D ) )
+    {
+        horizontal += 1.0f;
+    }
+
+    // pre-calculate acceleration
+    if ( horizontal != 0.0f || vertical != 0.0f )
+    {
+        d_acceleration = ACCELERATION *
+                         Vec::normalize( Vec2( horizontal, vertical ));
+    }
+    else
+    {
+        d_acceleration = Vec2( 0.0f, 0.0f );
+    }
+}
+
+void PlayerController::tick( float dt )
+{
+    using namespace sgdw;
+    using namespace gel::math;
+
+    World& w = World::inst();
+
+    // check if it meets the minimum velocity for movement
+    bool hasMinVelocity = Vec::length( d_velocity ) > MIN_VELOCITY;
+
+    // get minimum dimension
+    const sgds::RectangleBounds& b = d_subject->bounds();
+    float minDimension = Math::min( b.width(), b.height() );
+
+    // is it moving faster than cutoff speed?
+    // or is the acceleration greater than the static friction?
+    if ( hasMinVelocity ||
+         Vec::length( d_acceleration ) > STATIC_FRICTION )
+    {
+        // calculate the applied friction
+        Vec2 friction;
+        if ( hasMinVelocity )
+        {
+            friction = KINETIC_FRICTION * Vec::normalize( -d_velocity );
+        }
+        else
+        {
+            friction = Vec2( 0.0f );
+        }
+
+        // calculate change in velocity due to friction
+        Vec2 dVf = friction * dt;
+
+        // prevent dVf from being greater than the velocity
+        if ( Vec::length( dVf ) > Vec::length( d_velocity ) )
+        {
+            dVf = -d_velocity;
+        }
+
+        // apply forces to velocity
+        d_velocity += d_acceleration * dt + dVf;
+    }
+
+    // check if new velocity meets cutoff
+    if ( Vec::length( d_velocity ) > MIN_VELOCITY )
+    {
+        // calculate displacement
+        Vec2 dP = d_velocity * dt;
+
+        // clamp displacement to one bounds offset
+        if ( dP.x > MAX_OFFSET * minDimension ||
+             dP.y > MAX_OFFSET * minDimension )
+        {
+            dP = Vec::normalize( dP ) * ( MAX_OFFSET * minDimension );
+        }
+
+        // clamp movement to inside of the world
+        float nl = b.left() + dP.x;
+        float nt = b.top() + dP.y;
+        float nr = b.right() + dP.x;
+        float nb = b.bottom() + dP.y;
+
+        if ( nl < w.left() )
+        {
+            dP.x = b.left() - w.left();
+            d_velocity.x *= 0.05f;
+        }
+        else if ( nr > w.right() )
+        {
+            dP.x = w.right() - b.right();
+            d_velocity.x *= 0.05f;
+        }
+
+        if ( nt < w.top() )
+        {
+            dP.y = b.top() - w.top();
+            d_velocity.y *= 0.05f;
+        }
+        else if ( nb > w.bottom() )
+        {
+            dP.y = w.bottom() - b.bottom();
+            d_velocity.y *= 0.05f;
+        }
+
+        // move subject
+        d_subject->move( dP.x, dP.y );
+    }
+}
+
+void PlayerController::postTick()
+{
+}
+
+} // End nspc mgc
+
+} // End nspc StevensDev
---
+++ src/game/controllers/player_controller.h
@@ -0,0 +1,180 @@
+// player_controller.h
+//
+// REFERENCE:
+// WU stands for world units
+// 1 WU = the width of the world
+//
+// OU stands for object unit
+// 1 OU = the length the minimal width of the bounding box
+//
+// The minimal width is defined as min( width, height )
+//
+#ifndef INCLUDED_PLAYER_CONTROLLER
+#define INCLUDED_PLAYER_CONTROLLER
+
+#include <engine/scene/itickable.h>
+#include <engine/input/input.h>
+#include <engine/world/actor.h>
+#include <gel/math/vec.h>
+
+namespace StevensDev
+{
+
+namespace mgc
+{
+
+class PlayerController : public sgds::ITickable
+{
+  private:
+    // CONSTANTS
+    static constexpr float MAX_OFFSET = 1.0f;
+      // The actor's maximum allowed velocity (OU/sec).
+
+    static constexpr float MIN_VELOCITY = 50.0f;
+      // The minimum required velocity to be considered in motion (WU/sec).
+
+    static constexpr float ACCELERATION = 17500.0f;
+      // The actor's applied acceleration (WU/sec^2).
+
+    static constexpr float KINETIC_FRICTION = 6000.0f;
+      // The actor's observed kinetic friction deceleration (WU/sec^2).
+
+    static constexpr float STATIC_FRICTION = 7500.0f;
+      // The actor's observed static friction (WU/sec^2).
+
+    // MEMBERS
+    gel::math::Vec2 d_acceleration;
+      // The actor's current acceleration vector (OU/sec^2).
+
+    gel::math::Vec2 d_velocity;
+      // The actor's velocity vector (OU/sec).
+
+    sgdw::Actor* d_subject;
+      // The controller's subject (or in this case player).
+
+  public:
+    // CONSTRUCTORS
+    PlayerController();
+      // Constructs a new player controller without a subject.
+
+    PlayerController( sgdw::Actor* subject );
+      // Constructs a new player controller using the given actor as it's
+      // subject.
+
+    PlayerController( sgdw::Actor* subject,
+                      const gel::math::Vec2& acceleration,
+                      const gel::math::Vec2& velocity );
+      // Constructs a new player controller for the given subject using
+      // the initial speeds.
+
+    PlayerController( const PlayerController& controller );
+      // Constructs a copy of another player controller.
+
+    ~PlayerController();
+      // Destructs the controller.
+
+    // OPERATORS
+    PlayerController& operator=( const PlayerController& controller );
+      // Makes this a copy of the other controller.
+
+    // ACCESSOR FUNCTIONS
+    sgdw::Actor* subject() const;
+      // Gets a reference to the subject.
+
+    const gel::math::Vec2& acceleration() const;
+      // Gets the player's current acceleration.
+
+    const gel::math::Vec2& velocity() const;
+      // Gets the player's current velocity.
+
+    // MEMBER FUNCTIONS
+    virtual void preTick();
+      // Prepares for the update cycle.
+
+    virtual void tick( float dt );
+      // Updates the player's position.
+
+    virtual void postTick();
+      // Prepares for the next update cycle.
+};
+
+// FREE OPERATORS
+inline
+std::ostream& operator<<( std::ostream& stream,
+                          const PlayerController& controller )
+{
+    sgdd::JsonPrinter p( stream );
+    p.open().print( "subject_id", controller.subject()->id() ).close();
+    return stream;
+}
+
+// CONSTRUCTORS
+inline
+PlayerController::PlayerController() : d_acceleration(), d_velocity(),
+                                       d_subject( nullptr )
+{
+}
+
+inline
+PlayerController::PlayerController( sgdw::Actor* subject )
+    : d_acceleration(), d_velocity(), d_subject( subject )
+{
+}
+
+inline
+PlayerController::PlayerController( sgdw::Actor* subject,
+                                    const gel::math::Vec2& acceleration,
+                                    const gel::math::Vec2& velocity )
+    : d_acceleration( acceleration ), d_velocity( velocity ),
+      d_subject( subject )
+{
+}
+
+inline
+PlayerController::PlayerController( const PlayerController& controller )
+    : d_acceleration( controller.d_acceleration ),
+      d_velocity( controller.d_velocity ),
+      d_subject( controller.d_subject )
+{
+}
+
+inline
+PlayerController::~PlayerController()
+{
+}
+
+// OPERATORS
+inline
+PlayerController& PlayerController::operator=(
+    const PlayerController& controller )
+{
+    d_acceleration = controller.d_acceleration;
+    d_velocity = controller.d_velocity;
+    d_subject = controller.d_subject;
+
+    return *this;
+}
+
+inline
+sgdw::Actor* PlayerController::subject() const
+{
+    return d_subject;
+}
+
+inline
+const gel::math::Vec2& PlayerController::acceleration() const
+{
+    return d_acceleration;
+}
+
+inline
+const gel::math::Vec2& PlayerController::velocity() const
+{
+    return d_velocity;
+}
+
+} // End nspc mgc
+
+} // End nspc StevensDev
+
+#endif
